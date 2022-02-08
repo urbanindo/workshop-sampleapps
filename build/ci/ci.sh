6d28b2df7d7c7bfddd7b64bffc22e556c055f26a
Ridwan Arifandi <ridwan@99.co>
2022-02-08 14:29:45 +0700

    feat(ci.sh) : install gcloud (check_ridwan)

--- build/ci/ci.sh
+++ build/ci/ci.sh
@@ -10,6 +10,7 @@ PUSH=$4
 
 
 _main() {
+    _setup_tools
     _init
     _set_env_name
     case $CI_STAGE in
@@ -29,6 +30,20 @@ _main() {
             _usage
             ;;
     esac
+}
+
+_setup_tools() {
+    echo "Install Gcloud"
+    gcloud version || true
+    if [ ! -d "$HOME/google-cloud-sdk/bin" ]
+    then
+        rm -rf $HOME/google-cloud-sdk
+        export CLOUDSDK_CORE_DISABLE_PROMPTS=1
+        curl https://sdk.cloud.google.com | bash > /dev/null # Silence gcloud installation
+    fi
+    # Add gcloud to $PATH
+    source /home/travis/google-cloud-sdk/path.bash.inc
+    gcloud version
 }
 
 _set_env_name() {
